cb1c1de2e0a96dd2265424b8736c54cc26a8d551
Thomas Brus <thomas.brus@me.com>
2014-12-02 00:02:53 +0100

    Expanded analyzer a bit

--- 5-analyzing-results/analyze.rb
+++ 5-analyzing-results/analyze.rb
@@ -1,21 +1,6 @@
 require 'ostruct'
+require 'pry'
 require 'csv'
-
-filename = ARGV.fetch(0)
-
-results = CSV.table(filename).map { |result|
-  actual = { 'true' => true, 'false' => false }.fetch(result.fetch(:actual_outcome))
-  predicted = { 'true' => true, 'false' => false }.fetch(result.fetch(:predicted_outcome))
-
-  p1 = Float(result.fetch(:first_player_probability))
-  p2 = Float(result.fetch(:first_player_probability))
-
-  OpenStruct.new(result.to_h.merge({
-    actual: actual,
-    predicted: predicted,
-    probability_distribution: [p1, p2]
-  }))
-}
 
 class Summary
   def initialize(results)
@@ -77,4 +62,69 @@ class Summary
   end
 end
 
+class ResultWrapper
+  def initialize(csv_row)
+    @csv_row = csv_row
+  end
+
+  def actual
+    to_boolean(@csv_row.fetch(:actual_outcome))
+  end
+
+  def predicted
+    to_boolean(@csv_row.fetch(:predicted_outcome))
+  end
+
+  def probability_distribution
+    @csv_row.values_at(:first_player_probability, :second_player_probability).map(&method(:Float))
+  end
+
+  def number_of_turns
+    @csv_row.values_at(:first_player_number_of_turns, :second_player_number_of_turns).reduce(:+)
+  end
+
+  def method_missing(method, *args, &block)
+    @csv_row.fetch(method) { super }
+  end
+
+  def respond_to_missing?(method, *)
+    @csv_row.has_key?(method) || super
+  end
+
+  private
+
+  def to_boolean(thing)
+    { 'true' => true, 'false' => false }.fetch(thing)
+  end
+
+end
+
+filename = ARGV.fetch(0)
+results = CSV.table(filename).map(&ResultWrapper.method(:new))
+
+puts "# All turns"
 puts Summary.new(results)
+
+# sorted_results = results.select { |result| result.progress > 0 }.sort_by(&:progress)
+sorted_results = results.sort_by(&:number_of_turns)
+grouped_results = sorted_results.each_slice(1000)
+
+puts "\n# Partitioned by number of turns"
+puts grouped_results.map(&Summary.method(:new))
+
+suprising_results = results.select do |result|
+  (result.current_score_difference > 0) != result.actual &&
+    result.current_score_difference != 0
+end
+
+puts "\n# Only where abs(current_score_difference) != abs(final_score_difference)"
+puts Summary.new(suprising_results)
+
+more_suprising_results = results.select do |result|
+  (result.rating_difference > 0) != result.actual &&
+    result.rating_difference != 0
+end
+
+puts "\n# Only where abs(rating_difference) != abs(final_score_difference)"
+puts Summary.new(more_suprising_results)
+
